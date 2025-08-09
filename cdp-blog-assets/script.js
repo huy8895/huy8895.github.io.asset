efbfbf64998d7e850ee398a2d4dcef9faa131f80
huytvdev <huy8895@gmail.com>
2025-08-09 19:23:59 +0700

    add cdp asset

---
+++ cdp-blog-assets/script.js
@@ -0,0 +1,64 @@
+// Phần 1: Script điều khiển YouTube Player
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('ytplayer', {
+        events: {}
+    });
+}
+
+function jumpToTime(seconds) {
+    if (player && typeof player.seekTo === 'function') {
+        player.seekTo(seconds, true);
+        player.playVideo();
+    }
+}
+
+// Phần 2: Script chức năng của trang (chào mừng, tooltip, v.v.)
+function greetUser() {
+    const now = new Date();
+    const hour = now.getHours();
+    let greeting = "";
+
+    if (hour >= 5 && hour < 12) {
+        greeting = "Good morning!";
+    } else if (hour >= 12 && hour < 17) {
+        greeting = "Good afternoon!";
+    } else if (hour >= 17 && hour < 21) {
+        greeting = "Good evening!";
+    } else {
+        greeting = "Good night!";
+    }
+
+    document.getElementById("greeting").innerText = greeting;
+
+    const circles = document.querySelectorAll('.social div');
+    let index = 0;
+
+    function showTooltipSequentially() {
+        const current = circles[index];
+        current.classList.add('show-tooltip');
+
+        setTimeout(() => {
+            current.classList.remove('show-tooltip');
+            index = (index + 1) % circles.length;
+        }, 3000);
+    }
+
+    showTooltipSequentially();
+    setInterval(showTooltipSequentially, 10000);
+
+    document.querySelector('.youtube').addEventListener('click', function () {
+        window.open('https://www.youtube.com/@LearnChineseThroughPodcast', '_blank');
+    });
+
+    document.querySelector('.coffe').addEventListener('click', function () {
+        window.open('https://www.coff.ee/learnchinesepodcast', '_blank');
+    });
+
+    document.querySelector('.spotify-channel').addEventListener('click', function () {
+        window.open('https://open.spotify.com/show/1hZZO7dyrSnW4MHvvZIXz7?si=5a3751815fdb4d3a', '_blank');
+    });
+}
+
+// Gọi hàm greetUser khi trang tải xong
+window.onload = greetUser;
